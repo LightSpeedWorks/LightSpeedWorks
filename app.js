3d05527a622c0635085137390f02ddcc3e60e0b5
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2013-09-08 22:43:03 +0900

    upgrade express version 3.4.0

--- app.js
+++ app.js
@@ -20,23 +20,23 @@ var path = require('path');
 
 var app = express();
 
-app.configure(function (){
-  app.set('port', process.env.PORT || 3001);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.configure('development', function (){
+// development only
+if ('development' == app.get('env')) {
   app.use(express.errorHandler());
-});
+}
 
 app.get('/', routes.index);
 app.get('/test', test.index);
@@ -54,7 +54,7 @@ app.startDateTime = new Date();
 console.log(app.startDateTime);
 
 http.createServer(app).listen(app.get('port'), function (){
-  console.log("Express server listening http://localhost:" + app.get('port'));
+  console.log('Express server listening on port ' + app.get('port'));
 });
 
 require('./http-proxy-server-ninja');
