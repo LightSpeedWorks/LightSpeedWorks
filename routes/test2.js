eb29a2e7941f7b9d4bfb6014f3eb9e6365ce5c8a
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2013-09-12 21:48:31 +0900

    add misc output

--- routes/test2.js
+++ routes/test2.js
@@ -10,5 +10,9 @@ exports.index = function (req, res){
   res.render('test2', { title: '光速工房 LightSpeedWorks',
         req: req, res: res,
         process: process,
-        util: util });
+        util: util,
+        misc: {
+          file: __filename,
+          dir: __dirname}
+  });
 };
