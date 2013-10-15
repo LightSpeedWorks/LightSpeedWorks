b2cac53d2fa9667906a25d0ba510c551010bd3d3
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2013-10-16 00:33:22 +0900

    add local-app/app.js

---
+++ local-app/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', CONFIG.port || 3000);
+app.set('views', __dirname);
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// index
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Express (local)' });
+});
+
+// users
+app.get('/users', function (req, res) {
+  res.send('respond with a resource');
+});
+
+module.exports.app = app;
