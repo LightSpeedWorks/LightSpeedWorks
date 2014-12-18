c4ab436179d56079cf4e947046cf21a92a2e57af
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2014-12-18 13:44:34 +0900

    fix punicode and decodeURI

--- app-app/app.js
+++ app-app/app.js
@@ -35,11 +35,26 @@ app.get('/*', function (req, res) {
   if (name.indexOf('.lightspeedworks.') > 0)
     name = name.slice(0, name.indexOf('.lightspeedworks.'));
 
-  res.render('index', { title: 'Express (' + name + ')',
+  var name2 = punicode.toUnicode(name);
+  if (name !== name2)
+    name2 = name2 + ' <- ' + name;
+
+  var url2 = decodeURI(req.url);
+  if (req.url !== url2)
+    url2 = url2 + ' <- ' + req.url;
+
+  var host2 = punycode.toUnicode(req.headers.host);
+  if (req.headers.host !== host2)
+    host2 = host2 + ' <- ' + req.headers.host;
+
+  res.render('index', { title: 'Express (' + name2 + ')',
     punycode: punycode,
     url:  req.url,
+    url2:  url2,
     name: name,
+    name2: name2,
     host: req.headers.host,
+    host2: host2,
     util: require('util'),
     hdrs: req.headers });
 });
